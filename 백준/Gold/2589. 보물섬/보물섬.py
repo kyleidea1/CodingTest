42da7bd4e3df38e5e218596d77f86da907437796
kyleidea1 <99781357+kyleidea1@users.noreply.github.com>
2024-08-03 12:56:42 +0900

    [Gold V] Title: 보물섬, Time: 856 ms, Memory: 125044 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -2,39 +2,38 @@ import sys
 from collections import deque
 
 def bfs(x, y, matrix):
+    INF = 987654321
     q = deque([(x, y)])
-    distance = [[987654321] * len(matrix[0]) for _ in range(len(matrix))]
+    distance = [[INF] * len(matrix[0]) for _ in range(len(matrix))]
     distance[x][y] = 0
-    visited = [[False] * len(matrix[0]) for _ in range(len(matrix))]
-    visited[x][y] = True
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
 
     while q:
         x,y = q.popleft()
+        new_dist = distance[x][y] + 1
         for i in range(4):
             nx,ny = x + dx[i], y + dy[i]
             if nx < 0 or nx >= len(matrix) or ny < 0 or ny >= len(matrix[0]):
                 continue
-            if matrix[nx][ny] == 'L' and not visited[nx][ny]:
-                distance[nx][ny] = distance[x][y] + 1
-                visited[nx][ny] = True
+            if matrix[nx][ny] == 'L' and new_dist < distance[nx][ny]:
+                distance[nx][ny] = new_dist
                 q.append((nx, ny))
-    valid_distance = [num for row in distance for num in row if num != 987654321]
+    valid_distance = [num for row in distance for num in row if num != INF]
     return max(valid_distance)
 
 def main():
     input = sys.stdin.readline
     a, b = map(int, input().split())
     matrix = [input().strip() for _ in range(a)]
-    max_cnt = 0 
+    cnt_list = []
+
     for i in range(a):
         for j in range(b):
             if matrix[i][j] == 'L':
-                cnt = bfs(i,j,matrix)
-                if cnt > max_cnt:
-                    max_cnt = cnt
-    print(max_cnt)
+                cnt_list.append(bfs(i,j,matrix))
+    
+    print(max(cnt_list))
 
 if __name__ == "__main__":
     main()
