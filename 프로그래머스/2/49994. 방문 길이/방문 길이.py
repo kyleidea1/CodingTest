94728292b206706cd52813628f5089882128208b
Minhyeok Kim <99781357+kyleidea1@users.noreply.github.com>
2024-08-29 14:12:25 +0900

    [level 2] Title: 방문 길이, Time: 4.39 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -0,0 +1,14 @@
+def solution(dirs):
+    map = [[False]*10 for _ in range(10)]
+    x,y = (5,5)
+    loads = []
+    dir = {'U':(0,1), 'R':(1,0), 'D':(0,-1), 'L':(-1,0)}
+    for d in dirs:
+        tmp = [(x,y)]
+        if 0 <= x+dir[d][0] < 11 and 0 <= y+dir[d][1] < 11:
+            x += dir[d][0]
+            y += dir[d][1]
+            tmp.append((x,y))
+            if tmp not in loads and [tmp[1],tmp[0]] not in loads:
+                loads.append(tmp)
+    return len(loads)
