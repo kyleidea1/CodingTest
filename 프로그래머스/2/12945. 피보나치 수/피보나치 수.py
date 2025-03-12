017cc3158476db4afa16ab42cf4614f60e37c7d1
Minhyeok Kim <99781357+kyleidea1@users.noreply.github.com>
2025-03-12 16:59:52 +0900

    [level 2] Title: 피보나치 수, Time: 498.61 ms, Memory: 456 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,11 +1,6 @@
 def solution(n):
-    if n == 0:
-        ans = 0
-    elif n == 1:
-        ans = 1
-    else:
-        l = [0,1]
-        for i in range(n-1):
-            l.append(l[-1]+l[-2])
-        ans = l[-1]
-    return ans % 1234567
+    f = [0] * 100001
+    f[1] = 1
+    for i in range(2,n+1):
+        f[i] = f[i-2] + f[i-1]
+    return f[n] % 1234567
