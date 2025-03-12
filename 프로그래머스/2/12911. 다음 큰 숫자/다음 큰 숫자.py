1b4418b476150bc946abb279c949439a6b99caff
Minhyeok Kim <99781357+kyleidea1@users.noreply.github.com>
2025-03-12 16:53:48 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,8 +1,7 @@
 def solution(n):
-    num_of_one = bin(n)[2:].count('1')
+    num_of_one = str(bin(n)[2:]).count('1')
     while True:
         n += 1
-        new = bin(n)[2:].count('1')
-        if new == num_of_one:
+        if bin(n).count('1') == num_of_one:
             break
     return n
