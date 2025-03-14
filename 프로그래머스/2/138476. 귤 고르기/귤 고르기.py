10107663951df8832b4cbb8e04e49042711e86d0
Minhyeok Kim <99781357+kyleidea1@users.noreply.github.com>
2025-03-14 20:11:17 +0900

    [level 2] Title: 귤 고르기, Time: 65.62 ms, Memory: 21.4 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -5,10 +5,48 @@ def solution(k, tangerine):
             dic[t] = 1
         else:
             dic[t] += 1
-    desc = sorted(dic.values(), reverse = True)
-    sum = 0
-    for i in range(len(desc)):
-        sum += desc[i]
-        if sum >= k:
-            break
-    return i+1
+    dic = sorted(dic.items(), key = lambda x:x[1], reverse = True)
+    summ = 0
+    cnt = 0
+    for i in range(len(dic)):
+        if summ < k:
+            summ += dic[i][1]
+            cnt += 1
+    return cnt
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # dic = dict()
+    # for t in tangerine:
+    #     if t not in dic:
+    #         dic[t] = 1
+    #     else:
+    #         dic[t] += 1
+    # desc = sorted(dic.values(), reverse = True)
+    # sum = 0
+    # for i in range(len(desc)):
+    #     sum += desc[i]
+    #     if sum >= k:
+    #         break
+    # return i+1
