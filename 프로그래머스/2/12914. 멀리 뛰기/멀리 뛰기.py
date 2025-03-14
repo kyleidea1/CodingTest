d8699aed53e274c904a5b467221c8860f936afac
Minhyeok Kim <99781357+kyleidea1@users.noreply.github.com>
2025-03-14 21:48:57 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.44 ms, Memory: 9.48 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,12 +1,22 @@
 def solution(n):
     if n == 1:
         return 1
-    elif n == 2:
-        return 2
-    else:
-        dp = [0]*(n+1)
-        dp[1] = 1
-        dp[2] = 2
-        for i in range(3,n+1):
-            dp[i] = dp[i-2] + dp[i-1]
+    dp = [0] * (n+1)
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3,n+1):
+        dp[i] = dp[i-2]+dp[i-1]
     return dp[n] % 1234567
+    
+    
+    # if n == 1:
+    #     return 1
+    # elif n == 2:
+    #     return 2
+    # else:
+    #     dp = [0]*(n+1)
+    #     dp[1] = 1
+    #     dp[2] = 2
+    #     for i in range(3,n+1):
+    #         dp[i] = dp[i-2] + dp[i-1]
+    # return dp[n] % 1234567
