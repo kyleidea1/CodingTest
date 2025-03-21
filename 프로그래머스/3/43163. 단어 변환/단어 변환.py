3b6dc7e2e9d04221aecfa4a19228c16d957e5744
Minhyeok Kim <99781357+kyleidea1@users.noreply.github.com>
2025-03-21 19:33:11 +0900

    [level 3] Title: 단어 변환, Time: 0.70 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,26 +1,57 @@
 from collections import deque
 
 def solution(begin, target, words):
-    q = deque()
-    visited = []
-    cnt = 0
-    q.append((begin,cnt))
-    
+    q = deque([(begin,0)])
+    dic = dict()
+    for w in words:
+        dic[w] = True
+        
     while q:
-        cur,cnt = q.popleft()
+        cur, cnt = q.popleft()
         cnt += 1
-        for w in words:
-            if w not in visited:
-                if check(cur,w):
-                    if w == target:
-                        return cnt
-                    else:
-                        q.append((w,cnt))
-                        visited.append(w)
+        for w in dic.keys():
+            if dic[w] and compare(cur,w):
+                if w == target:
+                    return cnt
+                q.append((w,cnt))
+                dic[w] = False
     return 0
 
-def check(word1,word2):
+def compare(word1,word2):
     cnt = 0
     for i in range(len(word1)):
-        cnt += (0 if word1[i] == word2[i] else 1)
-    return cnt == 1
+        if word1[i] != word2[i]:
+            cnt += 1
+    return True if cnt == 1 else False
+    
+    
+    
+    
+    
+    
+    
+    
+# def solution(begin, target, words):
+#     q = deque()
+#     visited = []
+#     cnt = 0
+#     q.append((begin,cnt))
+    
+#     while q:
+#         cur,cnt = q.popleft()
+#         cnt += 1
+#         for w in words:
+#             if w not in visited:
+#                 if check(cur,w):
+#                     if w == target:
+#                         return cnt
+#                     else:
+#                         q.append((w,cnt))
+#                         visited.append(w)
+#     return 0
+
+# def check(word1,word2):
+#     cnt = 0
+#     for i in range(len(word1)):
+#         cnt += (0 if word1[i] == word2[i] else 1)
+#     return cnt == 1
