08abfb37ad16ded4ea9041f9c5dec06c1dcdf288
Minhyeok Kim <99781357+kyleidea1@users.noreply.github.com>
2025-05-02 19:19:53 +0900

    [level 1] Title: 기사단원의 무기, Time: 2221.17 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/README.md
+++ 프로그래머스/1/136798. 기사단원의 무기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 기사단원의 무기 - 136798 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798#) 
 
 ### 성능 요약
 
-메모리: 11 MB, 시간: 1096.18 ms
+메모리: 10.2 MB, 시간: 2221.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 02일 19:19:01
+2025년 05월 02일 19:19:51
 
 ### 문제 설명
 
--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,16 +1,9 @@
+def cf(n): # 공약수 출력
+    a = []
+    for i in range(1,int(n**0.5)+1):
+        if n%i == 0:
+            a.append(n//i)
+            a.append(i)
+    return len(set(a))
 def solution(number, limit, power):
-    l = [count_div(i) for i in range(1,number+1)]
-    return sum([i if i <= limit else power for i in l])
-
-def count_div(n):
-    if n == 1:
-        return 1
-    else:
-        cnt = 2
-        for i in range(2,int(n**(1/2)+1)):
-            if n % i == 0:
-                if n // i == i:
-                    cnt += 1
-                else:
-                    cnt += 2
-    return cnt
+    return sum([cf(i) if cf(i)<=limit else power for i in range(1,number+1)])
